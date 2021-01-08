ab4772b28b18fc26a2169bce5f238d39233d08b2
abhishek1411 <abhishek.t1411@gmail.com>
2021-01-08 14:36:15 +0100

    Rectify error in assigning state and label values over multiple graphs

--- rgnn_seird.py
+++ rgnn_seird.py
@@ -18,9 +18,10 @@ from torch.nn import Linear
 import torch.nn.functional as F
 from torch_geometric import utils
 from torch_geometric.nn import GCNConv,SAGEConv
-from torch.utils.data import TensorDataset
 
-from sklearn.preprocessing import StandardScaler
+
+
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.metrics import confusion_matrix
 
 
@@ -39,6 +40,7 @@ def time_since(since):
 
 ######## CLASS
 #Method 1 from paper:STRUCTURED SEQUENCE MODELING WITH GRAPH CONVOLUTIONAL RECURRENT NETWORKS
+
 class Recurrent_GCN(torch.nn.Module):
     def __init__(self, hidden_channels,dropout):
         # Init parent
@@ -95,8 +97,91 @@ class Recurrent_GCN(torch.nn.Module):
 
         return x_out
 
-    def init_hidden(self, batch_size=1):
-        return self.hidden_c.expand(self.layer_count, batch_size, self.hidden_channels).contiguous()
+    def init_hidden(self, batch_size):
+        weight = next(self.parameters()).data
+        hidden = weight.new(self.layer_count, batch_size, self.hidden_channels).zero_().to(device)
+        return hidden
+
+class Recurrent_GCN_1(torch.nn.Module):
+    def __init__(self, hidden_channels,dropout):
+        # Init parent
+        super(Recurrent_GCN_1, self).__init__()
+        torch.manual_seed(42)
+        self.dropout = dropout
+        self.hidden_channels = hidden_channels
+
+        # GCN layers: 2 message passing layers (to create embedding)
+        self.conv1 = GCNConv(num_features, self.hidden_channels)
+        self.conv2 = GCNConv(self.hidden_channels, self.hidden_channels)
+
+        self.layer_count = 1
+        self.hidden_c = nn.Parameter(torch.randn(self.layer_count, 1, self.hidden_channels).cuda(), requires_grad=True)
+        self.Recurrent_unit1 = nn.GRUCell(self.hidden_channels, self.hidden_channels)
+
+        # Output layer: is the classification layer
+        self.out = Linear(hidden_channels, 5)
+
+    def forward(self, _input):
+        x, edge_index, batch = _input.x, _input.edge_index, _input.batch
+        for k_lookback in range(lookback):
+            if k_lookback == 0:
+                next_hidden = self.init_hidden(batch_size=x.shape[0])[0]
+
+            x_gnn = self.conv1(x[:,k_lookback,:], edge_index)
+            x_gnn = x_gnn.relu() # the classical activation function
+            x_gnn = F.dropout(x_gnn, p=self.dropout, training=self.training)# and dropout to avoid overfitting
+
+            # Second Message Passing layer
+            x_gnn = self.conv2(x_gnn, edge_index)
+            x_gnn = x_gnn.relu()
+            x_gnn = F.dropout(x_gnn, p=self.dropout, training=self.training)
+
+
+            #RNN
+            next_hidden = self.Recurrent_unit1(x_gnn, next_hidden)
+
+        # Output layer: as in NN output activation function with a probability (to be a certain class) as ouput
+        x_gnn = F.softmax(self.out(next_hidden), dim=1) #is the classification layer
+
+        return x_gnn
+
+    # def init_hidden(self, batch_size=1):
+    #     return self.hidden_c.expand(self.layer_count, batch_size, self.hidden_channels).contiguous()
+
+    def init_hidden(self, batch_size):
+        weight = next(self.parameters()).data
+        hidden = weight.new(self.layer_count, batch_size, self.hidden_channels).zero_().to(device)
+        return hidden
+
+class GCN(torch.nn.Module):
+    def __init__(self, hidden_channels,dropout):
+        # Init parent
+        super(GCN, self).__init__()
+        torch.manual_seed(42)
+        self.dropout = dropout
+
+        # GCN layers: 2 message passing layers (to create embedding)
+        self.conv1 = GCNConv(num_features, hidden_channels)
+        self.conv2 = GCNConv(hidden_channels, hidden_channels)
+
+        # Output layer: is the classification layer
+        self.out = Linear(hidden_channels, 5)
+
+    def forward(self, _input):
+        x_in, edge_index, batch = _input.x, _input.edge_index, _input.batch
+        # First Message Passing layer: is equal as in NN with the edge info
+        x = self.conv1(x_in[:,-1,:], edge_index)
+        x = x.relu() # the classical activation function
+        x = F.dropout(x, p=self.dropout, training=self.training)# and dropout to avoid overfitting
+
+        # Second Message Passing layer
+        x = self.conv2(x, edge_index)
+        x = x.relu()
+        x = F.dropout(x, p=self.dropout, training=self.training)
+
+        # Output layer: as in NN output activation function with a probability (to be a certain class) as ouput
+        x = F.softmax(self.out(x), dim=1) #is the classification layer
+        return x
 ######## FUNCTIONS
 
 def train(_train_data):
@@ -154,11 +239,9 @@ def load_data_rnn(path, data_x, data_y, data_state,total_graphs):
         for i in range(num_days-lookback):
             start_idx = i*num_nodes + j*(num_nodes*num_days-lookback)
             end_idx = i*num_nodes + num_nodes + j*(num_nodes*num_days- lookback)
-            # data_list.append(Data(x=x[start_idx:end_idx,:,:], y=y[start_idx:end_idx],
-            #                       edge_index=edge_index.to(device), state=data_state[start_idx:end_idx]))
             data_list.append(Data(x=x[start_idx:end_idx,:,:], y=y[start_idx:end_idx],
-                state=data_state[start_idx:end_idx], graph_idx = loc))
-            # data_list.append(TensorDataset(x[start_idx:end_idx,:,:], y[start_idx:end_idx]))
+                                  edge_index=edge_index.to(device), state=data_state[start_idx:end_idx],
+                                  graph_idx = loc))
 
     return data_list
 ######## PREPROCESS DATA
@@ -200,12 +283,13 @@ class GNNDataset(InMemoryDataset):
         torch.save((data, slices), self.processed_paths[0])
 
 # Load data
-no_graphs = 25
-no_graphs_test = 10
+no_graphs = 1
+no_graphs_test = 1
 hidden_nodes = 0.2
 k_days = 15
 dropout_hidden = 0.3
-print(f'Evaluation: run on sequence RGNN')
+weight_scale = 1.2
+print(f'Evaluation: run rgnn with gcn network to check')
 print(f'Train graphs:{no_graphs},Test graphs:{no_graphs_test},'
       f'hidden_nodes:{hidden_nodes}, k_days:{k_days}')
 file_loc_train = './graphs'
@@ -216,7 +300,7 @@ start_idx_graph = 0
 df, num_nodes, num_days = load_graphs(no_graphs, hidden_nodes, file_loc_train, start_idx_graph,k_days)
 print(f'No.of_days_used:{num_days}')
 # feature scaling
-sc = StandardScaler()
+sc = MinMaxScaler()
 known_idx_train = np.where(df['state'] == 1)[0]
 scaler = sc.fit(df.values[known_idx_train,2:-2])
 df.values[known_idx_train,2:-2] = scaler.transform(df.values[known_idx_train,2:-2])
@@ -233,12 +317,9 @@ for j in range(no_graphs):
         start_idx = i-lookback + j*(num_nodes*num_days - lookback)
         end_idx = i + j*(num_nodes*num_days - lookback)
         inputs[start_idx] = df.values[start_idx:end_idx,2:7]
-        labels[start_idx] = df.values[i,7]
-        state_node[start_idx] = df.values[i,8]
-# inputs = inputs.reshape(-1,lookback,5)
-# labels = labels.reshape(-1,1)
+        labels[start_idx] = df.values[end_idx,7]
+        state_node[start_idx] = df.values[end_idx,8]
 
-#Change to create sequence for same realization
 # use GPU
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 # train_data_list = GNNDataset('./')
@@ -248,19 +329,19 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 train_data_list = load_data_rnn(file_loc_train, inputs, labels, state_node, no_graphs)
 num_features = train_data_list[-1].num_features
 train_indices = [id for id in range(len(train_data_list))]
-# random.shuffle(train_indices)
-batch_graphs = 2
-train_dataset = DataLoader(train_data_list, batch_size=batch_graphs*num_days, sampler=train_indices)
+random.shuffle(train_indices)
+train_dataset = DataLoader(train_data_list, batch_size=512, sampler=train_indices)
 
 num_features = 5
 # train_dataset.num_classes = 5 # S,E,I,R,D
 
 # Initialize Model
-model = Recurrent_GCN(hidden_channels=16, dropout=dropout_hidden)
+model = GCN(hidden_channels=32, dropout=dropout_hidden)
 print(model)
 model = model.to(device)
 
-learning_rate = 0.0005 # step for gradient descendent method for learning (?)
+
+learning_rate = 0.0003 # step for gradient descendent method for learning (?)
 decay = 5e-4 #decay of importance of learning rate
 # optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=decay)
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, amsgrad=True)
@@ -272,7 +353,8 @@ w2 = np.where(df['y'] == 2)[0].shape[0]
 w3 = np.where(df['y'] == 3)[0].shape[0]
 w4 = np.where(df['y'] == 4)[0].shape[0]
 max_w = max(w0,w1,w2,w3,w4)
-weight = torch.tensor([max_w/w0,max_w/w1,max_w/w2,max_w/w3,(max_w/w4)*0.5]).to(device)
+weight = torch.tensor([max_w/w0,(max_w/w1)*0.9*weight_scale,(max_w/w2)*weight_scale,(max_w/w3),(max_w/w4)]).to(device)
+print(weight)
 criterion = torch.nn.CrossEntropyLoss(weight=weight)
 # cross entropy compare probabilities, and we have probabilities because of softmax
 
@@ -281,7 +363,7 @@ del df
 del train_data_list
 losses = []
 start = time.time()
-for epoch in range(400):
+for epoch in range(500):
     loss = train(train_dataset)
     losses.append(loss)
     if epoch % 100 == 0:
@@ -313,14 +395,14 @@ for n_test in range(3):
     labels = np.zeros(len(df_test)-no_graphs_test*lookback)
     state_node = np.zeros(len(df_test)-no_graphs_test*lookback)
 
-    #use df.t
+    #TODO: Code repetition, remove it and use function
     for j in range(no_graphs_test):
         for i in range(lookback, int(num_days*num_nodes)):
             start_idx = i-lookback + j*(num_nodes*num_days - lookback)
             end_idx = i + j*(num_nodes*num_days - lookback)
             inputs[start_idx] = df_test.values[start_idx:end_idx,2:7]
-            labels[start_idx] = df_test.values[i,7]
-            state_node[start_idx] = df_test.values[i,8]
+            labels[start_idx] = df_test.values[end_idx,7]
+            state_node[start_idx] = df_test.values[end_idx,8]
     test_data_list = load_data_rnn(file_loc_test,inputs, labels, state_node,no_graphs_test)
     test_indices = [id for id in range(len(test_data_list))]
     test_dataset = DataLoader(test_data_list, batch_size=256, sampler=test_indices)
